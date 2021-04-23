553f600229bdf01f746e59dd078992c4c2980ce2
kasramalih <kasramalihi@gmail.com>
2021-04-24 04:28:28 +0430

    board and cell version 1.0

---
+++ AP2020/src/main/java/model/Board.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Board {
+    private Cell[] monsterZone = new Cell[5];
+    private Cell[] spellZone = new Cell[5];
+    private Cell fieldZone = new Cell();
+    private Graveyard graveyard = new Graveyard();
+    private Deck deck;
+    public void addCardToMonsterZone(Card card){
+        for (int i = 0; i < 5; i++) {
+            if(!monsterZone[i].isOccupied()) monsterZone[i].addCard(card);
+        }
+    }
+    public void addCardToSpellZone(Card card){
+        for (int i = 0; i < 5; i++) {
+            if(!spellZone[i].isOccupied()) spellZone[i].addCard(card);
+        }
+    }
+    public void sendToGraveYard(Card card){
+        //TODO
+    }
+    public Card getCardFromDeck(){
+        //TODO
+    }
+    public Card removeCardFromMonsterZone(Card card){
+        for (int i = 0; i < 5; i++) {
+            if(monsterZone[i].getCard().equals(card)){
+                return monsterZone[i].removeCard();
+            }
+        }
+        return null;
+    }
+    public Card removeCardFromSpellZone(Card card){
+        for (int i = 0; i < 5; i++) {
+            if (spellZone[i].getCard().equals(card)) {
+                return spellZone[i].removeCard();
+            }
+        }
+        return null;
+    }
+    public Card removeCardFromGraveYard(Card card){
+        //TODO
+        return card;
+    }
+    public void addCardToFieldZone(Card card){
+        if (!fieldZone.isOccupied()) fieldZone.addCard(card);
+    }
+    public Card removeCardFromFieldZone(Card card){
+        if (fieldZone.getCard().equals(card)) return fieldZone.removeCard();
+        return null;
+    }
+    public int getSumLevel(){
+        //TODO
+    }
+}
---
+++ AP2020/src/main/java/model/Cell.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Cell {
+    private Card card;
+    private State state;
+    private int roundCounter;
+    public boolean isOccupied(){
+        return this.card != null;
+    }
+    public Card removeCard(){
+        Card returningCard = this.card;
+        this.card = null;
+        this.roundCounter = 0 ;
+        this.state = null;
+        return returningCard;
+
+    }
+    public void addCard(Card card){
+        this.card = card;
+    }
+    public Card getCard(){
+        return this.card;
+    }
+    public boolean isFaceUp(){
+        return state.equals(State.FACE_UP_ATTACK) || state.equals(State.FACE_UP_DEFENCE);
+    }
+    public boolean isFaceDown(){
+        return state.equals(State.FACE_DOWN_ATTACK) || state.equals(State.FACE_DOWN_DEFENCE);
+    }
+    public boolean isAttack(){
+        return state.equals(State.FACE_UP_ATTACK) || state.equals(State.FACE_DOWN_ATTACK);
+    }
+    public boolean isDefence(){
+        return state.equals(State.FACE_UP_DEFENCE) || state.equals(State.FACE_DOWN_DEFENCE);
+    }
+    public int getRoundCounter(){
+        return this.roundCounter;
+    }
+    public void increaseRoundCounter(){
+        this.roundCounter++;
+    }
+}
