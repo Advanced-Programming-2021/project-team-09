eb19947fcb46c2820b2974d2699217f86eea6df7
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-04-24 23:58:00 +0430

    test csv file

--- AP2020/src/main/java/Main.java
+++ AP2020/src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.google.gson.Gson;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 import model.User;
+import java.util.Properties;
 
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -11,10 +12,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        String fileName = System.getProperty("os.name").startsWith("Windows")?"src\\resources\\card-infos\\SpellTrap.csv":"src/resources/card-infos/SpellTrap.csv";
         try {
             CSVReader reader = new CSVReader(new FileReader("src\\resources\\card-infos\\SpellTrap.csv"));
-            List<String[]> tmep = reader.readAll();
-            for (String[] a : tmep) System.out.println(a[0] + ".");
+            List<String[]> temp = reader.readAll();
+            for (String[] a : temp) System.out.println(a[0] + ".");
 
         } catch (IOException | CsvException e) {
 
