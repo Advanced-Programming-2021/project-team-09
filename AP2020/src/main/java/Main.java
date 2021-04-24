534bfd585256e867daf10acafbea7d8aa26a0cfc
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-04-24 23:41:22 +0430

    csv info getter updated. added card name checker to  csv checker.

--- AP2020/src/main/java/Main.java
+++ AP2020/src/main/java/Main.java
@@ -1,11 +1,24 @@
 import com.gilecode.yagson.YaGson;
 import com.google.gson.Gson;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import model.User;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            CSVReader reader = new CSVReader(new FileReader("src\\resources\\card-infos\\SpellTrap.csv"));
+            List<String[]> tmep = reader.readAll();
+            for (String[] a : tmep) System.out.println(a[0] + ".");
+
+        } catch (IOException | CsvException e) {
+
+        }
+        System.out.println();
     }
 }
