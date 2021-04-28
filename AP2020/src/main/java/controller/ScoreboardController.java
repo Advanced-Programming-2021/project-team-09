51634596d445c419eca3f51291789f57beb43026
kasramalih <kasramalihi@gmail.com>
2021-04-28 05:51:52 +0430

    dealing with scoreboard problems!

--- AP2020/src/main/java/controller/ScoreboardController.java
+++ AP2020/src/main/java/controller/ScoreboardController.java
@@ -1,13 +1,31 @@
 package controller;
 
-import java.util.Collections;
+
+import model.User;
+
+import java.util.ArrayList;
 
 public class ScoreboardController {
-    public void showScoreBoard(){
-        //TODO
-        ReadAndWriteDataBase.getAllUsers().sort(Collections.reverseOrder());
+    private ArrayList<User> sortedUsersByRank = ReadAndWriteDataBase.getAllUsers();
+    public String showScoreBoard(){
+        StringBuilder showingScoreboard = new StringBuilder();
+        for (User user: sortedUsersByRank) {
+            //ToDo
+        }
+         return showingScoreboard.toString();
     }
     public CurrentMenu showCurrentMenu(){
         return CurrentMenu.SHOP_MENU;
+    }
+    public void sortUsersByRank(){
+
+        int numberOfUsers = sortedUsersByRank.size();
+        for (int i = 0; i < numberOfUsers - 1; i++) {
+            for (int j = 0; j < numberOfUsers - i - 1; j++) {
+                if(sortedUsersByRank.get(j).getScore() > sortedUsersByRank.get(j + 1).getScore()){
+                    //ToDo
+                }
+            }
+        }
     }
 }
--- AP2020/src/main/java/view/ScoreboardMenu.java
+++ AP2020/src/main/java/view/ScoreboardMenu.java
@@ -1,4 +1,37 @@
 package view;
 
+import java.util.Scanner;
+
+import controller.ScoreboardController;
+import view.responses.ScoreboardMenuResponses;
 public class ScoreboardMenu {
+    private static ScoreboardMenu scoreboardMenu;
+    private final Scanner scanner;
+
+    private ScoreboardMenu(Scanner scanner){
+        this.scanner = scanner;
+    }
+    public static ScoreboardMenu getScoreboardMenu(Scanner scanner){
+        if(scoreboardMenu == null) scoreboardMenu = new ScoreboardMenu(scanner);
+        return scoreboardMenu;
+    }
+
+    public void run(){
+        String command;
+        while (true){
+            command = scanner.nextLine().trim();
+            if(command.matches("scoreboard show")) showScoreBoard();
+            else if(command.matches("back")) return;
+            else respond(ScoreboardMenuResponses.INVALID_COMMAND);
+        }
+    }
+    public void showScoreBoard(){
+        //what the fuck is wrong with you???
+    }
+
+    public void respond(ScoreboardMenuResponses response){
+        if(response.equals(ScoreboardMenuResponses.INVALID_COMMAND)) System.out.println("invalid command!");
+        else if(response.equals(ScoreboardMenuResponses.SHOWING_SCOREBOARD))
+            System.out.println();
+    }
 }
