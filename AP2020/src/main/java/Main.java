e3f663f7380d799082d6c61f0ba53a1cde6c5970
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-05-02 12:30:58 +0430

    no boogs fikhsed.

--- AP2020/src/main/java/Main.java
+++ AP2020/src/main/java/Main.java
@@ -1,16 +1,4 @@
-
-import controller.*;
-import model.card.spell_traps.Trap;
-import model.*;
-import com.gilecode.yagson.YaGson;
-import com.google.gson.Gson;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
-import model.User;
-import org.jetbrains.annotations.TestOnly;
 import view.LoginMenu;
-import java.util.Properties;
-
 
 public class Main {
     public static void main(String[] args) {
