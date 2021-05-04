a941339391d5109185db653acd82ebf88ee059f8
Siasor88 <ssrahimi2@gmail.com>
2021-05-04 16:55:03 +0430

    Cards!
    added few Cards functions!

--- AP2020/src/main/java/controller/MonsterEffectController.java
+++ AP2020/src/main/java/controller/MonsterEffectController.java
@@ -5,186 +5,215 @@ import model.Cell;
 import model.Game;
 import model.Limits;
 import model.card.Card;
+import model.deck.Graveyard;
+import view.CardEffectsView;
+import view.responses.CardEffectsResponses;
 
 public class MonsterEffectController {
 
-    public void CommandKnight(Game game, Card card){
+    public void CommandKnight(Game game, Card card) {
         Limits limits;
-        if (doesCardBelongsToPlayer(game,card)) limits = game.getPlayerLimits();
+        if (doesCardBelongsToPlayer(game, card)) limits = game.getPlayerLimits();
         else limits = game.getRivalLimits();
         limits.increaseATKAddition(400);
-        int cellNumber = getCellNumberOfMonster(game,card);
+        int cellNumber = getCellNumberOfMonster(game, card);
         limits.banAttackingToCell(cellNumber);
     }
 
-    public void BattleOX(){
+    public void YomiShip() {
 
     }
 
-    public void AxeRaider(){
+    public void HornImp() {
 
     }
 
-    public void YomiShip(){
+    public void SilverFang() {
 
     }
 
-    public void HornImp(){
+    public void Suijin() {
 
     }
 
-    public void SilverFang(){
+    public void Fireyarou() {
 
     }
 
-    public void Suijin(){
+    public void Curtainofthedarkones() {
 
     }
 
-    public void Fireyarou(){
+    public void FeralImp() {
 
     }
 
-    public void Curtainofthedarkones(){
+    public void Darkmagician() {
 
     }
 
-    public void FeralImp(){
+    public void Wattkid() {
 
     }
 
-    public void Darkmagician(){
+    public void Babydragon() {
 
     }
 
-    public void Wattkid(){
+    public void Herooftheeast() {
 
     }
 
-    public void Babydragon(){
+    public void Battlewarrior() {
 
     }
 
-    public void Herooftheeast(){
+    public void Crawlingdragon() {
 
     }
 
-    public void Battlewarrior(){
+    public void Flamemanipulator() {
 
     }
 
-    public void Crawlingdragon(){
+    public void BlueEyeswhitedragon() {
 
     }
 
-    public void Flamemanipulator(){
+    public void CrabTurtle() {
 
     }
 
-    public void BlueEyeswhitedragon(){
+    public void SkullGuardian() {
 
     }
 
-    public void CrabTurtle(){
+    public void SlotMachine() {
 
     }
 
-    public void SkullGuardian(){
+    public void Haniwa() {
 
     }
 
-    public void SlotMachine(){
+    public void ManEaterBug(Game game, Card card) {
+        Board board;
+        if (doesCardBelongsToPlayer(game,card)) board = game.getRivalBoard();
+        else board = game.getPlayerBoard();
 
+        int cellNumber = CardEffectsView.getCellNumber() - 1;
+        if (!isCellNumberValid(cellNumber)) CardEffectsView.respond(CardEffectsResponses.INVALID_CELL_NUMBER);
+        else {
+            Cell cell = board.getMonsterZone(cellNumber);
+            if (cell.isOccupied()) {
+                board.removeCardFromMonsterZone(cell.getCard());
+            } else CardEffectsView.respond(CardEffectsResponses.INVALID_CELL_NUMBER);
+        }
     }
 
-    public void Haniwa(){
-
-    }
-
-    public void ManEaterBug(){
-
-    }
-
-    public void GateGuardian(){
-
-    }
-
-    public void Scanner(){
-
-    }
-
-    public void Bitron(){
-
-    }
-
-    public void Marshmallon(){
-
-    }
-
-    public void BeastKingBarbaros(){
-
-    }
-
-    public void Texchanger(){
-
-    }
-
-    public void Leotron(){
-
-    }
-
-    public void TheCalculator(){
-
-    }
-
-    public void AlexandriteDragon(){
-
-    }
-
-    public void MirageDragon(){
-
-    }
-
-    public void HeraldofCreation(){
-
-    }
-
-    public void ExploderDragon(){
-
-    }
-
-    public void WarriorDaiGrepher(){
-
-    }
-
-    public void DarkBlade(){
-
-    }
-
-    public void Wattaildragon(){
-
-    }
-
-    public void TerratigertheEmpoweredWarrior(){
-
-    }
-
-    public void TheTricky(){
-
-    }
-
-    public void SpiralSerpent(){
-
-    }
-
-
-    public int getCellNumberOfMonster(Game game, Card card) {
+    public void GateGuardian(Game game,Card card) {
         Board board;
         if (doesCardBelongsToPlayer(game,card)) board = game.getPlayerBoard();
         else board = game.getRivalBoard();
+        int[] cellNumbers = CardEffectsView.getCellNumbers(3);
+        if (isCellNumberValid(cellNumbers[0]) &&
+            isCellNumberValid(cellNumbers[1]) &&
+            isCellNumberValid(cellNumbers[2])) {
+            Cell[] cells = new Cell[3];
+            cells[0] = board.getMonsterZone(cellNumbers[0]);
+            cells[1] = board.getMonsterZone(cellNumbers[1]);
+            cells[2] = board.getMonsterZone(cellNumbers[2]);
+            if (cells[0].isOccupied() && cells[1].isOccupied() && cells[2].isOccupied()) {
+                for (int i =0 ; i < cells.length; ++i) {
+                    board.removeCardFromMonsterZone(cells[i].getCard());
+                    board.addCardToMonsterZone(card);
+                }
+            }
+        }
+    }
+
+    public void Scanner(Game game, Card card) {
+        Board board;
+        if (doesCardBelongsToPlayer(game,card)) board = game.getRivalBoard();
+        else board = game.getPlayerBoard();
+        Graveyard graveyard = board.getGraveyard();
+        Card card1 = CardEffectsView.getCarFromGraveyard(graveyard);
+        if (card1 == null) return;
+        
+
+    }
+
+    public void Bitron() {
+
+    }
+
+    public void Marshmallon() {
+
+    }
+
+    public void BeastKingBarbaros() {
+
+    }
+
+    public void Texchanger() {
+
+    }
+
+    public void Leotron() {
+
+    }
+
+    public void TheCalculator() {
+
+    }
+
+    public void AlexandriteDragon() {
+
+    }
+
+    public void MirageDragon() {
+
+    }
+
+    public void HeraldofCreation() {
+
+    }
+
+    public void ExploderDragon() {
+
+    }
+
+    public void WarriorDaiGrepher() {
+
+    }
+
+    public void DarkBlade() {
+
+    }
+
+    public void Wattaildragon() {
+
+    }
+
+    public void TerratigertheEmpoweredWarrior() {
+
+    }
+
+    public void TheTricky() {
+
+    }
+
+    public void SpiralSerpent() {
+
+    }
+
+    public int getCellNumberOfMonster(Game game, Card card) {
+        Board board;
+        if (doesCardBelongsToPlayer(game, card)) board = game.getPlayerBoard();
+        else board = game.getRivalBoard();
         Cell[] cells = board.getMonsterZone();
-        for (int i = 0; i < cells.length ; i++) {
-            if (cells[i].getCard().equals(card)) return i+1;
+        for (int i = 0; i < cells.length; i++) {
+            if (cells[i].getCard().equals(card)) return i + 1;
         }
         return 0;
     }
@@ -198,6 +227,14 @@ public class MonsterEffectController {
         for (Cell cell : cells) {
             if (cell.getCard().equals(card)) return true;
         }
+        for(Card card1 : game.getPlayerHandCards()) {
+            if (card1.equals(card)) return true;
+        }
         return false;
     }
+
+    public boolean isCellNumberValid(int cellNumber) {
+        return cellNumber >= 0 && cellNumber < 5;
+    }
+
 }
---
+++ AP2020/src/main/java/view/responses/CardEffectsResponses.java
@@ -0,0 +1,6 @@
+package view.responses;
+
+public enum CardEffectsResponses {
+    INVALID_CELL_NUMBER,
+
+}
