bb04e406d36e02f8dc83ce23f58d050864b8fc9b
Siasor88 <ssrahimi2@gmail.com>
2021-05-07 22:12:13 +0430

    regex fix!

--- AP2020/src/main/java/view/regexes/DeckMenuRegex.java
+++ AP2020/src/main/java/view/regexes/DeckMenuRegex.java
@@ -4,10 +4,10 @@ abstract public class DeckMenuRegex {
     public static final String creatDeckRegex = "^deck create (?<deckName>\\w+)$";
     public static final String deleteDeckRegex = "^deck delete (?<deckName>\\w+)$";
     public static final String activeDeckRegex = "^deck set-activate (?<deckName>\\w+)$";
-    public static final String addCardToMainDeckRegex = "^deck add-card --card (?<cardName>[\\w ]+) --deck (?<deckName>\\w+)$";
-    public static final String addCardToSideDeckRegex = "^deck add-card --card (?<cardName>[\\w ]+) --deck (?<deckName>\\w+) --side$";
-    public static final String removeCardFromMainDeckRegex = "^deck rm-card --card (?<cardName>[\\w ]+) --deck (?<deckName>\\w+)$";
-    public static final String removeCardFromSideDeckRegex = "^deck rm-card --card (?<cardName>[\\w ]+) --deck (?<deckName>\\w+) --side$";
+    public static final String addCardToMainDeckRegex = "^deck add-card --card (?<cardName>[\\w -,]+) --deck (?<deckName>\\w+)$";
+    public static final String addCardToSideDeckRegex = "^deck add-card --card (?<cardName>[\\w -,]+) --deck (?<deckName>\\w+) --side$";
+    public static final String removeCardFromMainDeckRegex = "^deck rm-card --card (?<cardName>[\\w -,]+) --deck (?<deckName>\\w+)$";
+    public static final String removeCardFromSideDeckRegex = "^deck rm-card --card (?<cardName>[\\w -,]+) --deck (?<deckName>\\w+) --side$";
 
     public static final String showAllDecksRegex = "^deck show --all$";
     public static final String showAllCardsRegex = "^deck show --cards$";
