4a09d487780a4a095e8ec01af4d20806d25aba58
kasramalih <kasramalihi@gmail.com>
2021-05-07 21:27:15 +0430

    some spellEffects added

---
+++ AP2020/src/main/java/view/duelMenu/GameMenu.java
@@ -0,0 +1,102 @@
+package view.duelMenu;
+
+import controller.LoginMenuController;
+import controller.database.ReadAndWriteDataBase;
+import model.User;
+import view.LoginMenu;
+import view.regexes.GameMenuRegex;
+import view.responses.GameMenuResponses;
+import view.responses.StartingGameResponses;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
+public class GameMenu {
+    private final Scanner scanner;
+    private static GameMenu gameMenu;
+
+    private GameMenu(Scanner scanner) {
+        this.scanner = scanner;
+    }
+    public static GameMenu getInstance(Scanner scanner){
+        if(gameMenu == null) gameMenu = new GameMenu(scanner);
+        return gameMenu;
+    }
+
+    public void run(){
+        String command;
+        while (true){
+            command = scanner.nextLine().trim();
+            if(GameMenuRegex.doesItDuelNewPlayerCommand(command))
+                duelNewPlayer(command);
+            else if (command.matches("help"))
+                showHelp();
+            else if (command.matches("menu show-current"))
+                respond(StartingGameResponses.CURRENT_MENU_DUEL_MENU);
+            else if (command.matches("menu exit"))
+                return;
+            else respond(StartingGameResponses.INVALID_COMMAND);
+        }
+    }
+    private void duelNewPlayer(String command){
+        HashMap<String,String> data = parseDuelNewPlayerData(command);
+        String username = data.get("username");
+        String rounds = data.get("rounds");
+        User currentUser = LoginMenuController.getCurrentUser();
+        if (currentUser.getUsername().equals(username))
+            respond(StartingGameResponses.THERE_IS_NO_PLAYER_WITH_THIS_USERNAME);
+        else {
+            User rivalUser = ReadAndWriteDataBase.getUser(username+".json");
+            if (rivalUser == null) respond(StartingGameResponses.THERE_IS_NO_PLAYER_WITH_THIS_USERNAME);
+            else if (rivalUser.getActiveDeck() == null)
+                System.out.println(username + " has no active deck");
+            else if (!rivalUser.getActiveDeck().isValid())
+                System.out.println(username +"'s deck is invalid");
+            else if(rounds.equals("1"))
+                singleRoundGame(currentUser, rivalUser);
+            else if(rounds.equals("3"))
+                tripleRoundGame(currentUser, rivalUser);
+            else respond(StartingGameResponses.NUMBER_OF_ROUNDS_IS_NOT_SUPPORTED);
+        }
+
+    }
+    public void singleRoundGame(User player, User rival){
+
+    }
+    public void tripleRoundGame(User player, User rival){
+
+    }
+    public void singlePlayerGame(){
+
+    }
+    private void respond(StartingGameResponses response){
+        if (response.equals(StartingGameResponses.INVALID_COMMAND))
+            System.out.println("invalid command!");
+        else if (response.equals(StartingGameResponses.CURRENT_MENU_DUEL_MENU))
+            System.out.println("duel menu");
+        else if (response.equals(StartingGameResponses.NUMBER_OF_ROUNDS_IS_NOT_SUPPORTED))
+            System.out.println("number of rounds is not supported");
+        else if (response.equals(StartingGameResponses.THERE_IS_NO_PLAYER_WITH_THIS_USERNAME))
+            System.out.println("there is no player with this username");
+    }
+    private HashMap<String,String> parseDuelNewPlayerData(String command){
+        HashMap<String,String> parsedData = new HashMap<>();
+        Matcher matcher = GameMenuRegex.getRightMatcherForDuelNewPlayer(command);
+        if(matcher.find()){
+            parsedData.put("username", matcher.group("username"));
+            parsedData.put("rounds", matcher.group("rounds"));
+        }
+        return parsedData;
+    }
+    public void showHelp(){
+        StringBuilder help = new StringBuilder();
+        help.append("duel new --second-player <player2 username> --rounds<1/3>\n");
+        help.append("menu show-current\n");
+        help.append("menu exit\n");
+        help.append("\n");
+        help.append("shortcut:\n");
+        help.append("duel new -s-p <player2 username> -r<1/3>");
+        System.out.println(help.toString());
+    }
+}
