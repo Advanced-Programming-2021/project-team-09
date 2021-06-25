4ee0006397679d583c61a2d192589e121cd13bf3
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-06-26 01:05:50 +0430

    oneroundgame tests.

---
+++ AP2020/src/test/java/OneRoundGameTest.java
@@ -0,0 +1,136 @@
+public class OneRoundGameTest extends TestEssentials{
+
+    public void startTest() {
+        String command = "user login -u mir -p mir\n" +
+                "menu enter duel\n" +
+                "duel -n -s-p mmd -r 1\n" +
+                "dice\n" +
+                "higher\n" +
+                "show table\n" +
+                "card show -s\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "send to graveyard\n" +
+                "select -h 1\n" +
+                "send to graveyard\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "select -h 7\n" +
+                "summon\n" +
+                "select -h 6\n" +
+                "summon\n" +
+                "select -h 5\n" +
+                "summon\n" +
+                "select -h 4\n" +
+                "summon\n" +
+                "select -h 3\n" +
+                "summon\n" +
+                "select -h 2\n" +
+                "summon\n" +
+                "select -h 1\n" +
+                "summon\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "next phase\n" +
+                "select -h 7\n" +
+                "summon\n" +
+                "select -h 6\n" +
+                "summon\n" +
+                "select -h 5\n" +
+                "summon\n" +
+                "select -h 4\n" +
+                "summon\n" +
+                "select -h 3\n" +
+                "summon\n" +
+                "select -h 2\n" +
+                "summon\n" +
+                "select -h 1\n" +
+                "summon\n";
+
+    }
+}
