ee851af263334c26855e31461e3f0453ad9d78ae
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-07-03 17:01:04 +0430

    coin mini game added

--- AP2020/src/main/java/main/Main.java
+++ AP2020/src/main/java/main/Main.java
@@ -38,6 +38,7 @@ import javafx.stage.Stage;
 import model.User;
 import model.game.MiniGame;
 import view.graphics.Menu;
+import view.graphics.duelgraphics.MiniGameCoin;
 import view.graphics.duelgraphics.MiniGameDice;
 import view.graphics.duelgraphics.MiniGameRockPaperScissors;
 
@@ -64,6 +65,6 @@ public class Main extends Application {
         stage.show();
         User user = new User("ali", "ali", "ali");
         User user2 = new User("mamad", "mamad", "mamad");
-//        new MiniGameDice(new MiniGame(user, user2), true);
+        new MiniGameCoin(new MiniGame(user, user2));
     }
 }
