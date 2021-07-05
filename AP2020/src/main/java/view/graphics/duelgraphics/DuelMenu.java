b41a24610789180998421d5570003d8f9a8e0e0e
kasramalih <kasramalihi@gmail.com>
2021-07-05 21:59:43 +0430

    boom

--- AP2020/src/main/java/view/graphics/duelgraphics/DuelMenu.java
+++ AP2020/src/main/java/view/graphics/duelgraphics/DuelMenu.java
@@ -2,11 +2,14 @@ package view.graphics.duelgraphics;
 
 
 import controller.LoginMenuController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Popup;
 import main.Main;
 import model.User;
@@ -77,31 +80,71 @@ public class DuelMenu extends Menu {
         User player = LoginMenuController.getCurrentUser();
         if (player.getActiveDeck() == null){
             Popup noActiveDeckPopup = new Popup();
-            Label label = new Label("you have no active deck");
-            label.setStyle(" -fx-background-color: red;" +
-                    " -fx-font-family: Chalkboard;" +
-                    " -fx-text-fill: white;" +
-                    " -fx-border-radius: 15");
-            label.setMinWidth(80);
-            label.setMinHeight(45);
-            noActiveDeckPopup.getContent().add(label);
-            noActiveDeckPopup.show(Main.stage);
-            return false;
+            HBox hBox = new HBox(10);
+
+            Label label = new Label("you don't have any active deck!");
+            return makePopUp(noActiveDeckPopup, hBox, label);
         }
         else if (!player.getActiveDeck().isValid()){
             Popup noValidDeckPopUp = new Popup();
+            HBox hBox = new HBox(10);
             Label label = new Label("your deck is invalid");
-            label.setStyle(" -fx-background-color: red;" +
-                    " -fx-font-family: Chalkboard;" +
-                    " -fx-border-radius: 15;" +
-                    " -fx-text-fill: white");
-            label.setMinWidth(80);
-            label.setMinHeight(45);
-            noValidDeckPopUp.getContent().add(label);
-            noValidDeckPopUp.show(Main.stage);
-            return false;
+
+            return makePopUp(noValidDeckPopUp, hBox, label);
         }
         else
             return true;
+    }
+
+    private boolean makePopUp(Popup noValidDeckPopUp, HBox hBox, Label label) {
+        hBox.setStyle(" -fx-background-color: white;" +
+                " -fx-border-color: grey;" +
+                " -fx-border-radius: 13;");
+        label.setStyle(" -fx-background-color: transparent;" +
+                " -fx-font-family: Chalkboard;" +
+                " -fx-text-fill: black;" +
+                " -fx-border-radius: 15");
+        label.setMinWidth(80);
+        label.setMinHeight(45);
+
+        Button hide = new Button("hide");
+        hide.setCursor(javafx.scene.Cursor.HAND);
+        hide.setStyle(" -fx-border-radius: 50;" +
+                " -fx-font-family: Chalkboard;" +
+                " -fx-text-fill: white;" +
+                " -fx-background-color: black;");
+
+        Button goToDeck = new Button("go to deck");
+        goToDeck.setCursor(javafx.scene.Cursor.HAND);
+        goToDeck.setStyle(" -fx-border-radius: 50;" +
+                " -fx-font-family: Chalkboard;" +
+                " -fx-text-fill: white;" +
+                " -fx-background-color: black;" +
+                " -fx-wrap-text: true");
+
+        hide.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                noValidDeckPopUp.hide();
+            }
+        });
+        goToDeck.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                noValidDeckPopUp.hide();
+                goToDeck();
+            }
+        });
+
+
+        hBox.getChildren().addAll(goToDeck, label, hide);
+        noValidDeckPopUp.getContent().add(hBox);
+        noValidDeckPopUp.show(Main.stage);
+        return false;
+    }
+    public void goToDeck(){
+
     }
 }
